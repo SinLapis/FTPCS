3bf6ef8ef8c00959d376509ff924f66bf252fd65
sinlapis <truthdragon@morgirl.org>
2016-05-02 23:16:05 +0800

    add functions: list, size, retr

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import socket
+import re
 
 __author__ = 'SinLapis'
 
@@ -6,7 +7,26 @@ class Client():
     def __init__(self):
         self.conn_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # todo exception
-        self.server_addr = ("127.0.0.1", 2121)
+        self.server_addr = ('127.0.0.1', 2121)
+        self.DOWNLOAD_PATH = './download/'
+
+    def extract_code(self, receive):
+        return receive[0:3]
+
+    def extract_message(self, message):
+        patten = re.compile(r'(.*) (.*)')
+        result = patten.match(message)
+        if result is not None:
+            command, addition = result.groups()
+        else:
+            command, addition = ('', '')
+        return command, addition
+
+    def extract_address(self, receive):
+        patten = re.compile(r'.*\((.*)\)')
+        result = patten.match(receive).groups()[0]
+        port = int(result)
+        return ('127.0.0.1', port)
 
     def start(self):
         self.conn_fd.connect(self.server_addr)
@@ -14,8 +34,46 @@ class Client():
         while True:
             message = input()
             self.conn_fd.send(message.encode())
-            print(self.conn_fd.recv(1024).decode())
+            receive = self.conn_fd.recv(1024).decode()
+            print(receive)
             if message == 'quit':
                 self.conn_fd.close()
-                print('Client has closed the connection.')
+                print('<client>Client has closed the connection.')
                 break
+            return_code = self.extract_code(receive)
+            command, addition = self.extract_message(message)
+            if return_code == '150':
+                data_addr = self.extract_address(receive)
+                data_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                if command == 'retr':
+                    file = open(self.DOWNLOAD_PATH + addition, 'w')
+                    data_fd.connect(data_addr)
+                    while True:
+                        buf = data_fd.recv(1024)
+                        if(len(buf)!=0):
+                            file.write(buf.decode())
+                        else:
+                            data_fd.close()
+                            break
+                    file.close()
+                    self.conn_fd.send('retrc'.encode())
+                    print(self.conn_fd.recv(1024).decode())
+                    print('<client>' + addition + ' has been downloaded.')
+                elif command == 'stor':
+                    file = open(addition)
+                    data_fd.connect(data_addr)
+                    buf = file.read(1024)
+                    while buf != '':
+                        data_fd.send(buf)
+                        buf = file.read(1024)
+                    data_fd.close()
+                    file.close()
+                    self.conn_fd.send('storc')
+                    print(self.conn_fd.recv(1024).decode())
+                    print('<client>' + addition + ' has been uploaded.')
+
+
+
+
+
+
--- server/server.py
+++ server/server.py
@@ -2,6 +2,10 @@ import socket
 import select
 import errno
 import re
+import os
+import stat
+import threading
+import random
 from server import status
 
 __author__ = 'SinLapis'
@@ -16,19 +20,29 @@ class Server():
         self.cmd_socket.setblocking(0)
         self.cmd_epoll = select.epoll()
         self.cmd_epoll.register(self.cmd_socket.fileno(), select.EPOLLIN)
+
         self.connections = {}
         self.addresses = {}
         self.messages = {}
         self.login_info = {}
+        self.work_path = {}
         self.need_username = set()
         self.need_password = set()
+        self.logged_in = set()
+        self.transferring = set()
+
         self.func_key = {
             'user': self.user,
-            'pass': self.pwd,
+            'pass': self.pw,
+            'list': self.ls,
+            'size': self.size,
+            'retr': self.retr,
         }
         self.user_info = {
             'admin': 'admin',
         }
+
+        self.FTP_ROOT = '/home/sinlapis/ftproot'
 
     def extract(self, message):
         patten = re.compile(r'(.*) (.*)')
@@ -40,7 +54,6 @@ class Server():
         return t
 
     def user(self, username, fd):
-        message = ''
         if fd in self.need_username:
             self.login_info[fd] = username
             try:
@@ -53,13 +66,14 @@ class Server():
             message = status.implement_error
         return message
 
-    def pwd(self, password, fd):
-        message = ''
+    def pw(self, password, fd):
         if fd in self.need_password:
             username = self.login_info[fd]
             if username in self.user_info:
                 if self.user_info[username] == password:
                     message = status.login_success
+                    self.logged_in.add(fd)
+                    self.work_path[fd] = self.FTP_ROOT + '/'
                 else:
                     message = status.login_error
                     self.need_username.add(fd)
@@ -74,13 +88,86 @@ class Server():
             message = status.implement_error
         return message
 
+    def ls(self, path, fd):
+        if fd in self.logged_in:
+            dirs = []
+            files = []
+            full_path = self.FTP_ROOT + path
+            self.work_path[fd] = full_path
+            try:
+                names = os.listdir(full_path)
+                for name in names:
+                    if stat.S_ISDIR(os.stat(full_path + name).st_mode):
+                        dirs.append(name)
+                    else:
+                        files.append(name)
+                message = status.command_implement
+                message += '\ndirs:'
+                for dir in dirs:
+                    message += '\n\t' + dir
+                message += '\nfiles:'
+                for file in files:
+                    message += '\n\t' + file
+            except FileNotFoundError:
+                message = status.unavailable_file
+        else:
+            message = status.need_account
+        return message
+
+    def size(self, filename, fd):
+        if fd in self.logged_in:
+            try:
+                message = status.command_implement + '\n'
+                message += str(os.stat(self.work_path[fd] + filename).st_size)
+            except FileNotFoundError:
+                message = status.unavailable_file
+        else:
+            message = status.need_account
+        return message
+
+    def retr(self, filename, fd):
+        if fd in self.logged_in:
+            self.transferring.add(fd)
+            data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            data_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            port = random.randrange(2122, 65535)
+            while True:
+                try:
+                    data_sock.bind(('', port))
+                    break
+                except socket.error:
+                    port = random.randrange(2122, 65535)
+            threading.Thread(target=self.retr_transfer,
+                             args=(self.work_path[fd] + filename,
+                                   data_sock, )).start()
+            port = '(' + str(port) + ')'
+            message = status.transfer_ready + port
+        else:
+            message = status.need_account
+        return message
+
+    def retr_transfer(self, file_path, data_sock):
+
+        data_sock.listen(1)
+        file = open(file_path)
+        data_fd, addr = data_sock.accept()
+        buf = file.read(1024)
+        while buf != '':
+            data_fd.send(buf.encode())
+            buf = file.read(1024)
+        file.close()
+        data_fd.close()
+        data_sock.close()
+
     def delete(self, fd):
         try:
             del self.login_info[fd]
             del self.messages[fd]
             del self.connections[fd]
+            del self.work_path[fd]
             self.need_username.remove(fd)
             self.need_password.remove(fd)
+            self.logged_in.remove(fd)
         except:
             pass
 
@@ -126,17 +213,29 @@ class Server():
                 # reading event
                 elif events & select.EPOLLIN:
                     message = self.read(self.connections[fd], fd)
+                    print(message)
                     if message == 'quit':
                         self.write(self.connections[fd], status.quit)
                         self.cmd_epoll.unregister(fd)
                         self.connections[fd].close()
                         self.delete(fd)
+                    # todo transfer status
+                    elif (fd in self.transferring) and (message == 'retrc' or
+                                                                message == 'storc'):
+                        self.transferring.remove(fd)
+                        self.messages[fd] = status.transfer_complete
+                        try:
+                            self.cmd_epoll.modify(fd, select.EPOLLET | select.EPOLLOUT)
+                        except:
+                            pass
                     else:
                         result = self.extract(message)
                         if result is not None:
                             command, addition = result
                             if command in self.func_key:
                                 self.messages[fd] = self.func_key[command](addition, fd)
+                            else:
+                                self.messages[fd] = status.command_error
                         else:
                             self.messages[fd] = status.command_error
                         # todo complete this block
